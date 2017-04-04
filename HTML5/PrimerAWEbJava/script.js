8b2119893677c3418d141f1999965565b0454c45
Cristobal <cristobalws@gmail.com>
2017-04-03 22:29:18 -0600

    Pasando elementos al azar

---
+++ HTML5/PrimerAWEbJava/script.js
@@ -0,0 +1,35 @@
+//JavaScript Document
+window.onload=ejecuta;
+window.onload=ejecuta2;
+alert("Este es el segundo wuuuuuu");
+
+function ejecuta(){
+	document.getElementsByTagName("p")[0].onclick=saludo;
+	document.getElementById("Elemento").onclick=saludos;
+}
+
+
+function saludo(){
+alert("Este es el saludooooooou");
+
+}
+function saludos(){
+alert("Este es el saludooooooou");
+
+}
+
+function saludo2(){
+alert("Este es el saludooooooou   22222222222222");
+
+}
+
+function saludo3(){
+alert("Este es el saludooooooou   3333333333333");
+
+}
+
+function saludo4(){
+alert("Este es el saludooooooou   444444444444444");
+
+}
+
